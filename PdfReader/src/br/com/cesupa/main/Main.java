e133a4705a72664909bce23f1e4ab4264658b4a5
Renan <renanfigcarneiro@gmail.com>
2019-12-16 17:54:47 -0300

    Código vasculando o pdf e passando pagina por parametro pro OCR

--- PdfReader/src/br/com/cesupa/main/Main.java
+++ PdfReader/src/br/com/cesupa/main/Main.java
@@ -1,10 +1,19 @@
 package br.com.cesupa.main;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.tools.imageio.ImageIOUtil;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 
 
 public class Main {
@@ -16,17 +25,39 @@ public class Main {
 		PDFTextStripper().getText(doc); 
 	 }
 	//END
-	public static void main(String[] args) throws IOException {
-		
+	public static void main(String[] args) throws IOException, TesseractException {
 		
 		//USA TEXT EXTRACT
 		
 		 String text;
-		  
-		 try { text = getText(new
-		 File("path to pdf file"));
-		 System.out.println(text); } catch (IOException e) {
-			 e.printStackTrace();
+		 PDFTextStripper reader = new PDFTextStripper();
+		 Tesseract instance = new Tesseract();
+		 instance.setLanguage("por");
+		 instance.setDatapath("D:\\Codigos\\Java\\iaGroupWorkspace\\PDF_Reader_JAVA\\PdfReader\\tessdata");
+		 try { 
+			//text = getText(new File("D:\\Downloads\\Processo_0000315-12.2018.5.08.0208.pdf"));
+			//System.out.println(text); 
+			 PDDocument doc = PDDocument.load(new File("D:\\Downloads\\Processo_0000315-12.2018.5.08.0208.pdf"));
+			 //int numero = doc.getNumberOfPages();
+			 //System.out.println(numero);
+			 for (int i = 0; i <= doc.getNumberOfPages(); i++) {
+				 reader.setStartPage(i);
+				 reader.setEndPage(i);
+				 reader.getText(doc);
+				 if (reader.getText(doc).contains("de2ae66") && !reader.getText(doc).contains("SUMÁRIO")) {
+					// System.out.println(reader.getText(doc));
+					 PDFRenderer renderer = new PDFRenderer(doc);
+					 BufferedImage image = renderer.renderImageWithDPI(i - 1, 300);
+					 //ImageIO.write(image, "JPEG", new File("D:\\Downloads\\texto.jpg"));
+					 //File file = new File("D:\\Downloads\\texto.jpg");
+					 String result = instance.doOCR(image);
+					 System.out.println(result);
+				 }
+			 }
+			 
+		 } 
+		 catch (IOException e) {
+			 //e.printStackTrace();
 		 }
 		 
 		//END
