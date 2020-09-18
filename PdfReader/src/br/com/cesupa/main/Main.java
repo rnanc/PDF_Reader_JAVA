b652ca905bd617ef49fc201dbd32bd6a53385f07
Renan <renanfigcarneiro@gmail.com>
2020-09-18 20:21:15 -0300

    docs: update README

--- PdfReader/src/br/com/cesupa/main/Main.java
+++ PdfReader/src/br/com/cesupa/main/Main.java
@@ -18,7 +18,7 @@ import net.sourceforge.tess4j.TesseractException;
 
 public class Main {
 	
-	//USA TEXT EXTRACT
+	//TEXT EXTRACT
 	
 	 static String getText(File pdfFile) throws IOException {	  
 		 PDDocument doc = PDDocument.load(pdfFile); return new
@@ -27,27 +27,28 @@ public class Main {
 	//END
 	public static void main(String[] args) throws IOException, TesseractException {
 		
-		//USA TEXT EXTRACT
+		//OCR
 		
 		 String text;
 		 PDFTextStripper reader = new PDFTextStripper();
 		 Tesseract instance = new Tesseract();
 		 instance.setLanguage("por");
-		 instance.setDatapath("D:\\Codigos\\Java\\iaGroupWorkspace\\PDF_Reader_JAVA\\PdfReader\\tessdata");
+		 instance.setDatapath("C:\\Users\\renan\\git\\PDF_Reader_JAVA\\PdfReader\\tessdata");
 		 try { 
 			//text = getText(new File("D:\\Downloads\\Processo_0000315-12.2018.5.08.0208.pdf"));
 			//System.out.println(text); 
-			 PDDocument doc = PDDocument.load(new File("D:\\Downloads\\Processo_0000315-12.2018.5.08.0208.pdf"));
+			 PDDocument doc = PDDocument.load(new File("D:\\Downloads\\Retoma_Para_16_09.pdf"));
 			 //int numero = doc.getNumberOfPages();
 			 //System.out.println(numero);
 			 for (int i = 0; i <= doc.getNumberOfPages(); i++) {
 				 reader.setStartPage(i);
 				 reader.setEndPage(i);
 				 //reader.getText(doc);
-			 if (reader.getText(doc).contains("Procuração") && reader.getText(doc).contains("SUMÁRIO")) {
+			 if (reader.getText(doc).contains("CAPÍTULO III")) {
 					 System.out.println(reader.getText(doc));
+					 System.out.println("PDFREADER");
 				}
-				 if (reader.getText(doc).contains("de2ae66") && !reader.getText(doc).contains("SUMÁRIO")) {
+				 if (reader.getText(doc).contains("CAPÍTULO III") ) {
 					// System.out.println(reader.getText(doc));
 					 PDFRenderer renderer = new PDFRenderer(doc);
 					 BufferedImage image = renderer.renderImageWithDPI(i - 1, 300);
@@ -55,6 +56,7 @@ public class Main {
 					 //File file = new File("D:\\Downloads\\texto.jpg");
 					 String result = instance.doOCR(image);
 					 System.out.println(result);
+					 System.out.println("ORC");
 				 }
 			 }
 			 doc.close();
---
+++ README.md
@@ -0,0 +1,45 @@
+<h1>:page_with_curl: PDF and Image Reader :page_with_curl: </h1>
+
+## Requirements
+- PdfBox
+- Tess4J-3.4.8
+
+## Instalation
+
+Clone this repository with the command:
+
+`$ git clone https://github.com/rnanc/PDF_Reader_JAVA.git`
+
+Import as a `maven project` and wait for it to build dependencies
+
+Change this path to your the right one for you system, this folder comes inside the project.
+
+<img src="assets/path-demo.jpg" width="50%">
+
+## Usage
+
+Insert the file you would like to read
+
+<img src="assets/file-demo.jpg" width="50%">
+
+Search for a word in PdfBox method:
+
+<img src="assets/pdf-box.jpg" width="50%">
+
+Search for a word in OCR method:
+
+<img src="assets/orc-demo.jpg" width="50%">
+
+## Outcome
+
+Check your console for both outcomes:
+
+### PDFBOX
+
+<img src="assets/pdfoutcome.jpg" width="50%">
+
+### OCR
+
+<img src="assets/ocr-outcome.jpg" width="50%">
+
+> Results for OCR may vary on color, quality or dpi settings for the pdf or image file affecting the final result
